fa76607769cfc30cb2fe4ecd691fd60acd3b7a46
half bucket <34878559+jiamingla@users.noreply.github.com>
2023-06-20 02:24:30 +0800

    feat: validate every api by JOI

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const Koa = require("koa");
-const Router = require("koa-router");
+const Router = require("koa-joi-router");
+const date_format = require("@joi/date");
+Joi = Router.Joi;
+Joi = Joi.extend(date_format);
+// https://community.magento.com/t5/Magento-1-x-Technical-Issues/Unknown-date-format-neither-date-nor-time-in-yyyy-MM-dd-HH-mm-ss/td-p/445733
 const { koaBody } = require("koa-body");
 const views = require("koa-views");
 const path = require("path");
 
 const mvdis = require("./mvdis_crawler.js");
 const dmvNoList = require("./dmvNo.json");
+
 const app = new Koa();
-const router = new Router();
+const router = Router();
 
 app.use(koaBody());
 
@@ -55,6 +60,37 @@ GET /results 取得場次報名結果
 DELETE /results 取消報名
 */
 
+const schema_licenseTypeCode = Joi.number()
+    .integer()
+    .min(2)
+    .max(3)
+    .example("3")
+    .description("licenseTypeCode")
+    .required();
+const schema_DateStr = Joi.date().format("YYYY-MM-DD").raw().required();
+const schema_dmvNo = Joi.string()
+    .pattern(
+        new RegExp(
+            "^.*21|25|26|30|31|33|40|41|43|44|45|46|50|51|52|53|54|60|61|62|63|64|65|70|71|72|73|74|75|76|80|81|82|83|84.*$"
+        )
+    )
+    .length(2)
+    .example("41")
+    .description("dmvNo")
+    .required();
+const schema_id = Joi.number()
+    .integer()
+    .min(1)
+    .max(5)
+    .example("3")
+    .description("secId,divId")
+    .required();
+const schema_idNo = Joi.string().length(10).required();
+
+const schema_name = Joi.string().required();
+const schema_contactTel = Joi.string().length(10).required();
+const schema_email = Joi.string().email().required();
+const schema_reservationPK = Joi.string().length(7).required();
 router
     .get("/", async (ctx) => {
         let title = "台灣駕照預約考試多語友善版";
@@ -63,109 +99,125 @@ router
             dmvNoList,
         });
     })
-    .get("/sessions", async (ctx) => {
-        let licenseTypeCode = 0;
-        let expectExamDateStr = "";
-        let dmvNo = 0;
-        try {
+    .get("/sessions", {
+        validate: {
+            query: {
+                licenseTypeCode: schema_licenseTypeCode,
+                expectExamDateStr: schema_DateStr,
+                dmvNo: schema_dmvNo,
+            },
+        },
+        handler: async (ctx) => {
             // 把資料分別存在變數裡
             licenseTypeCode = parseInt(ctx.query.licenseTypeCode);
             expectExamDateStr = ctx.query.expectExamDateStr;
             dmvNo = parseInt(ctx.query.dmvNo);
-        } catch (error) {
-            console.error(error);
-            ctx.body = "參數錯誤";
-            ctx.status = 400;
-        }
-        // 2023-05-16 -> 1120516
-        expectExamDateStr = UTC_to_ROC(expectExamDateStr);
-        console.log(expectExamDateStr);
-        // TODO: 其實可以使用JOI來寫輸入值檢查，不至於這麼搞剛
-        const motorVehiclesOfficeIdList = [
-            21, 25, 26, 30, 31, 33, 40, 41, 43, 44, 45, 46, 50, 51, 52, 53, 54,
-            60, 61, 62, 63, 64, 65, 70, 71, 72, 73, 74, 75, 76, 80, 81, 82, 83,
-            84,
-        ];
-        const licenseTypeCodeList = [2, 3];
-        if (
-            expectExamDateStr.length === 7 &&
-            licenseTypeCodeList.includes(licenseTypeCode) &&
-            motorVehiclesOfficeIdList.includes(dmvNo) === true
-        ) {
+            // 2023-05-16 -> 1120516
+            expectExamDateStr = UTC_to_ROC(expectExamDateStr);
+            console.log(expectExamDateStr);
+
             const result = await mvdis.locations_query(
                 licenseTypeCode,
                 expectExamDateStr,
                 dmvNo
             );
             ctx.body = result;
-        } else {
-            // 如果有欄位沒有填，就依照文件回傳 400
-            ctx.body = "出事了阿伯";
-            ctx.status = 400;
-        }
+        },
     })
-    .post("/sessions", async (ctx) => {
-        // 把資料分別存在變數裡
-        const {
-            licenseTypeCode,
-            secDateStr,
-            dmvNo,
-            secId,
-            divId,
-            idNo,
-            birthdayStr,
-            name,
-            contactTel,
-            email,
-        } = ctx.request.body;
-        const result = await mvdis.sign_up(
-            licenseTypeCode,
-            secDateStr,
-            dmvNo,
-            secId,
-            divId,
-            idNo,
-            UTC_to_ROC(birthdayStr),
-            name,
-            contactTel,
-            email
-        );
-        if (result === false) {
-            console.log("報名失敗");
-            ctx.body = "Failed";
-            ctx.status = 400;
-        } else {
-            console.log("報名成功");
-            ctx.body = "OK";
-        }
+    .post("/sessions", {
+        validate: {
+            type: "json",
+            body: {
+                licenseTypeCode: schema_licenseTypeCode,
+                secDateStr: schema_DateStr,
+                dmvNo: schema_dmvNo,
+                secId: schema_id,
+                divId: schema_id,
+                idNo: schema_idNo,
+                birthdayStr: schema_DateStr,
+                name: schema_name,
+                contactTel: schema_contactTel,
+                email: schema_email,
+            },
+        },
+        handler: async (ctx) => {
+            // 把資料分別存在變數裡
+            const {
+                licenseTypeCode,
+                secDateStr,
+                dmvNo,
+                secId,
+                divId,
+                idNo,
+                birthdayStr,
+                name,
+                contactTel,
+                email,
+            } = ctx.request.body;
+            const result = await mvdis.sign_up(
+                licenseTypeCode,
+                secDateStr,
+                dmvNo,
+                secId,
+                divId,
+                idNo,
+                UTC_to_ROC(birthdayStr),
+                name,
+                contactTel,
+                email
+            );
+            if (result === false) {
+                console.log("報名失敗");
+                ctx.body = "Failed";
+                ctx.status = 400;
+            } else {
+                console.log("報名成功");
+                ctx.body = "OK";
+            }
+        },
     })
-    .get("/results", async (ctx) => {
-        // 把資料分別存在變數裡
-        const idNo = ctx.query.idNo;
-        const birthdayStr = UTC_to_ROC(ctx.query.birthdayStr);
+    .get("/results", {
+        validate: {
+            query: {
+                idNo: schema_idNo,
+                birthdayStr: schema_DateStr,
+            },
+        },
+        handler: async (ctx) => {
+            // 把資料分別存在變數裡
+            const idNo = ctx.query.idNo;
+            const birthdayStr = UTC_to_ROC(ctx.query.birthdayStr);
 
-        const result = await mvdis.query_query(idNo, birthdayStr);
-        if (result === false) {
-            ctx.body = "查詢失敗";
-            ctx.status = 400;
-        } else {
-            // TODO: 處理這個查詢條件查不到紀錄時該回的訊息格式 -> 這段邏輯寫在哪裡比較好?
-            ctx.body = result;
-        }
+            const result = await mvdis.query_query(idNo, birthdayStr);
+            if (result === false) {
+                ctx.body = "查詢失敗";
+                ctx.status = 400;
+            } else {
+                // TODO: 處理這個查詢條件查不到紀錄時該回的訊息格式 -> 這段邏輯寫在哪裡比較好?
+                ctx.body = result;
+            }
+        },
     })
-    .delete("/results", async (ctx) => {
-        // 把資料分別存在變數裡
-        const reservationPK = ctx.query.reservationPK;
-        const result = await mvdis.query_cancel(reservationPK);
-        if (result === false) {
-            ctx.body = "取消報名失敗";
-            ctx.status = 400;
-        } else {
-            ctx.body = "取消報名成功";
-        }
+    .delete("/results", {
+        validate: {
+            query: {
+                reservationPK: schema_reservationPK,
+            },
+        },
+        handler: async (ctx) => {
+            // 把資料分別存在變數裡
+            const reservationPK = ctx.query.reservationPK;
+            const result = await mvdis.query_cancel(reservationPK);
+            if (result === false) {
+                ctx.body = "取消報名失敗";
+                ctx.status = 400;
+            } else {
+                ctx.body = "取消報名成功";
+            }
+        },
     });
 
-app.use(router.routes());
+app.use(router.middleware());
 
 // 2023-05-16 -> 1120516
 // TODO: 船進去的質一定要是西元年格式，或是多檢查判斷去檢查輸入的是西元還是民國格式
