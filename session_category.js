88c472d9e1f6247168b807e0404cdd12c93da02d
half bucket <34878559+jiamingla@users.noreply.github.com>
2023-06-27 00:54:01 +0800

    feat: let api could collect chinese description

--- session_category.js
+++ session_category.js
@@ -14,28 +14,42 @@ result:
     }
 ]
 */
-let description_list = [];
-let result_list = [];
+function category(search_table, dmvName, licenseTypeCode) {
+    let description_list = [];
+    let result_list = [];
 
-for (const [key, value] of Object.entries(session_example)) {
-    const description = value["description"];
-    console.log(`${description}`);
-    if (!description_list.includes(description)) {
-        description_list.push(description);
-        result_list.push({
-            category_id: 0,
-            count: 1,
-            description: description,
-            date_list: [value["date"]],
-        });
-    } else {
-        // Add date
-        for (const [key, result] of Object.entries(result_list)) {
-            if (description === result["description"]) {
-                result["count"]++;
-                result["date_list"].push(value["date"]);
+    for (const [key, value] of Object.entries(search_table)) {
+        const description = value["description"];
+        let category_id;
+        if (value["secId"] || value["divId"]) {
+            category_id = `${value["secId"]}_${value["divId"]}`;
+        }
+        console.log(`${description}`);
+        if (!description_list.includes(description)) {
+            description_list.push(description);
+            result_list.push({
+                dmvName: dmvName,
+                licenseTypeCode: licenseTypeCode,
+                category_id: category_id ?? 0,
+                count: 1,
+                description: description,
+                date_list: [value["date"]],
+            });
+        } else {
+            // Add date
+            for (const [key, result] of Object.entries(result_list)) {
+                if (description === result["description"]) {
+                    result["category_id"] = category_id ?? 0;
+                    result["count"]++;
+                    result["date_list"].push(value["date"]);
+                }
             }
         }
     }
+    console.group(result_list);
+    return result_list;
 }
-console.group(result_list);
+
+category(session_example);
+
+module.exports = category;
